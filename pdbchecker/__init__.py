54e4645e5123fde1cee225770c55d1ee8476dd68
M. Eric Irrgang <eirrgang@umich.edu>
2015-10-17 15:50:06 +0200

    add Adam's presentation for reference'

--- pdbchecker/__init__.py
+++ pdbchecker/__init__.py
@@ -18,3 +18,29 @@ def get_apolar(u):
     selector = ' or '.join(['(resname {} and name {})'.format(pair[0], pair[1]) for pair in types['Apolar']])
     apolar = u.atoms.select_atoms(selector)
     return apolar
+
+def get_polar_mixt(u):
+    types = json.load(open('types.json'))
+    selector = ' or '.join(['(resname {} and name {})'.format(pair[0], pair[1]) for pair in types['Polar Mixt']])
+    selection = u.atoms.select_atoms(selector)
+    return selection
+def get_polar_donor_ionic_positive(u):
+    types = json.load(open('types.json'))
+    selector = ' or '.join(['(resname {} and name {})'.format(pair[0], pair[1]) for pair in types['Polar Donnor, Ionic Positive']])
+    selection = u.atoms.select_atoms(selector)
+    return selection
+def get_polar_acceptor_ionic_negative(u):
+    types = json.load(open('types.json'))
+    selector = ' or '.join(['(resname {} and name {})'.format(pair[0], pair[1]) for pair in types['Polar Acceptor, Ionic Negative']])
+    selection = u.atoms.select_atoms(selector)
+    return selection
+def get_polar_donor(u):
+    types = json.load(open('types.json'))
+    selector = ' or '.join(['(resname {} and name {})'.format(pair[0], pair[1]) for pair in types['Polar Donor']])
+    selection = u.atoms.select_atoms(selector)
+    return selection
+def get_polar_acceptor(u):
+    types = json.load(open('types.json'))
+    selector = ' or '.join(['(resname {} and name {})'.format(pair[0], pair[1]) for pair in types['Polar Acceptor']])
+    selection = u.atoms.select_atoms(selector)
+    return selection
